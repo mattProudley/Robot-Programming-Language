02d5ea234dad2e54ec120b6ae6efe46fa584cb74
mattProudley <mattjamesproudley@gmail.com>
2024-04-11 16:34:43 +0100

    Untracked files

--- file_handling.py
+++ file_handling.py
@@ -2,6 +2,7 @@
 from tkinter import filedialog
 
 
+# Function handles saving files
 def save_file(data):
     """Function to save data as a text file."""
     # Prompt the user to select a file path for saving
@@ -9,7 +10,7 @@ def save_file(data):
     # If a file path is provided by the user
     if file_path:
         # Write the text to the selected file and return message
-        with open(file_path, "w") as f:
+        with open(file_path, "w") as f:  # File is closed after the with statement
             f.write(data)
         return "File saved"
     # Else return error
@@ -17,6 +18,7 @@ def save_file(data):
         return "File not saved"
 
 
+# Function handles opening files
 def open_file():
     """Function to open a file and return data"""
     # Prompt the user to select a file to open
@@ -24,7 +26,7 @@ def open_file():
     # If a file path is provided by the user
     if file_path:
         # Open the selected file in read mode and return data then message
-        with open(file_path, "r") as file:
+        with open(file_path, "r") as file:  # File is closed after the with statement
             data = file.read()
             return data, "File Opened"
     # Else returns null data and error
